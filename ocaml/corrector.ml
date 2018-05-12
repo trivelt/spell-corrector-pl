a705492b5bc9d4a8cb6c75490ab0535d69711158
Maciej Michalec <maciej.michalec@gmail.com>
2018-05-12 11:03:39 +0200

    Removed unused code

--- ocaml/corrector.ml
+++ ocaml/corrector.ml
@@ -43,47 +43,17 @@ let rec splits word index l_splits =
     let l_splits = (word_L, word_R) :: l_splits in
     if next_index = length+1 then l_splits else splits word next_index l_splits
 
-
-let rec print_list = function 
-[] -> print_endline ""
-| e::l -> 
-    let a, b = e in 
-    print_string a;
-    print_string "-" ;
-    print_string b;
-    print_string ", ";
-    print_list l
-
-let rec print_list_sec = function 
-[] -> print_endline ""
-| e::l -> 
-    print_string e;
-    print_string ", ";
-    print_list_sec l
-
-
-let rec deletes l_splits l_deletes =
-    match l_splits with
-        | [] -> l_deletes
-        | hd::tl -> 
-            let word_L, word_R = hd in 
-            let new_word_R = String.drop_prefix word_R 1 in
-            let word_del = word_L ^ new_word_R in
-            deletes tl (word_del::l_deletes)
-
-
-let deletes l_splits = 
+let deletes l_splits =
     List.map ~f:(fun x -> let x_L, x_R = x in x_L ^ (String.drop_prefix x_R 1)) l_splits
 
-
-let get_char text n = 
+let get_char text n =
     Char.to_string (String.get text n)
 
-let transposes l_splits = 
+let transposes l_splits =
     List.filter_map ~f:(fun x ->
-        let x_L, x_R = x in 
+        let x_L, x_R = x in
         if String.length x_R > 1 then
-            Some (x_L ^ 
+            Some (x_L ^
             get_char x_R 1 ^
             get_char x_R 0 ^
             (String.drop_prefix x_R 2))
@@ -91,18 +61,17 @@ let transposes l_splits =
             None
     ) l_splits
 
-
 let rec change_splits l_splits l_output f =
     let letters = String.to_list "abcdefghijklmnopqrstuvwxyz" |> List.map ~f:(fun x -> Char.to_string x) in
     match l_splits with
             | [] -> l_output
             | (x_L, x_R)::tl -> change_splits tl (l_output @ List.fold letters ~init:[] ~f:(f x_L x_R)) f
 
-let inserts l_splits = 
+let inserts l_splits =
     change_splits l_splits [] (fun x_L x_R acc c -> (x_L ^ c ^ x_R) :: acc)
 
-let replaces l_splits = 
-    let replace_letter x_L x_R acc c = 
+let replaces l_splits =
+    let replace_letter x_L x_R acc c =
         if String.length x_R < 1 then acc
         else
             let word_R = String.drop_prefix x_R 1 in
@@ -126,21 +95,18 @@ let flatten list =
     | []     :: t -> aux accu t
     | (x::y) :: t -> aux (x::accu) (y::t) in
   List.rev (aux [] list)
-(* let flatten = List.fold_left ~init:[] ~f:(fun acc l -> acc @ l) *)
 
-let edits1 word = 
+let edits1 word =
     let splitted = splits word 0 [] in
     let deletes = deletes splitted in
     let transposes = transposes splitted in
     let replaces = replaces splitted in
     let inserts = inserts splitted in
-(*    let candidates = deletes @ transposes @ replaces @ inserts in *)
-    let candidates = append deletes (append transposes (append replaces inserts)) in
+    let candidates = deletes @ transposes @ replaces @ inserts in
     let set = List.fold_left candidates ~f:(fun set elem -> StringSet.add set elem) ~init:StringSet.empty in
     StringSet.to_list set
 
-
-let edits2 word = 
+let edits2 word =
     let l_edits1 = edits1 word in
     let l_edits2 = List.map ~f:(fun word -> edits1 word) l_edits1 in
     flatten l_edits2
@@ -176,18 +142,13 @@ let sort_by_P known_words candidates =
 
 let correction word known_words =
     let candidates = get_candidates word known_words in
-(*    let _ = print_list_sec candidates in *)
     let sorted_candidates = sort_by_P known_words candidates in
     if (List.length sorted_candidates) > 0 then
         List.hd_exn sorted_candidates
     else
         word
 
-(*    let is_known = Hashtbl.find known_words word in
-    word
-*)
-
-let is_interactive_mode = 
+let is_interactive_mode =
     match Array.find Sys.argv ~f:(fun arg -> arg = "-i") with
         | None -> false
         | Some _ -> true
